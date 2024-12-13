dd5a571f0c93d182229a1a8263de09eb9dbd3c20
MaximoXv <maximoibarra164@gmail.com>
2024-12-13 10:25:10 -0300

    install axios and create api-logic

---
+++ src/components/card-container.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import { Card } from './card'
+import axios from 'axios';
+
+interface Character{
+    id: string;
+    name: string;
+    species: string;
+    image: string;
+}
+
+export const CardContainer = () => {
+
+    const [characters, setCharacters] = useState<Character[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(()=>{
+        const fetchCharacters = async () => {
+            try {
+                const response = await axios.get('https://rickandmortyapi.com/api/character');
+                setCharacters(response.data.results);
+                setLoading(false);
+              } catch (error) {
+                console.error('Error al obtener los datos:', error);
+                setLoading(false);
+              }  
+        };
+        fetchCharacters();
+    }, []);
+
+    if (loading) {
+        return <div className="text-center text-xl font-bold">Cargando...</div>;
+      }
+
+  return (
+    <div className='flex w-full flex-wrap'>
+        {characters?characters.map((character)=>{
+            return <Card name={character.name} specie={character.species} image={character.image}/>
+        }):[]}
+
+    </div>
+  )
+}
